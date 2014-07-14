d4f13450066aceebe9a9c3df7d1c102a32e4b344
wzip <william.zipse@ecologicalforester.com>
2014-07-14 18:15:28 -0400

    Calculations and processing complete

--- FuelsProcessor.java
+++ FuelsProcessor.java
@@ -38,18 +38,22 @@ public class FuelsProcessor
 			boolean standFirstLn = true;
 			boolean tsectFirstLn = true;
 			boolean dFirstLn = true;
+			//other variables
+			int nStands = 0;
 			//Create and initialize stand file reader
 			BufferedReader standBr = new BufferedReader(new FileReader(standFile));
 			//Create file writer for outputs
 			BufferedWriter out = new BufferedWriter(new FileWriter(outputFile));
 			//write header to output file------------------------------------------------------------
-			out.write("StandId,AvgPerSlope,SlopeCorFactor,AvgLitterIn,AvgDuffIn,LitterTonsPerAc,"+
-			"DuffTonsPerAc\n");
+			out.write("StandId,AvgPerSlope,SlopeCorFactor,AvgLitterIn,AvgDuffIn,LitterTonsAc,"+
+			"DuffTonsAc,QtrInTonsAc,OneInTonsAc,ThreeInTonsAc,ThreeInPlsSTonsAc,"+
+			"ThreeInPlsRTonsAc,TotTonsAc\n");
 			
 			//read stand file line by line
 			while((standLine = standBr.readLine()) != null)
 			{
 				BufferedReader tsectBr = new BufferedReader(new FileReader(transectFile));
+				BufferedReader dBr = new BufferedReader(new FileReader (dFile));
 				//skip first line containing headers
 				if (standFirstLn)
 					standFirstLn = false;
@@ -63,14 +67,20 @@ public class FuelsProcessor
 					double standLitterAvgSum = 0.0;
 					double tsectDuffAvg = 0.0;
 					double standDuffAvgSum = 0.0;
+					int standNQtrIn = 0;
+					int standN1In = 0;
+					int standN3In = 0;
+					double sumD2s = 0.0;
+					double sumD2r =0.0;
+					
+					
+					++nStands;
 					//get tokens for each stand in line
 					String[] standTokens = standLine.split(DELIMITER);
 					stand.setStandId(standTokens[0]);
 					stand.setPlane1(Integer.parseInt(standTokens[1]));
 					stand.setPlane3(Integer.parseInt(standTokens[2]));
 					stand.setPlaneThreeP((Integer.parseInt(standTokens[3])));
-					//test output
-					System.out.println("StandID: "+stand.getStandId());
 					
 					//loop to process transect data for each stand
 					while((tsectLine = tsectBr.readLine()) != null)
@@ -93,43 +103,89 @@ public class FuelsProcessor
 										tsect.setDuffDepth1(Double.parseDouble(tsectTokens[6]));
 										tsect.setDuffDepth2(Double.parseDouble(tsectTokens[7]));
 										tsect.setDuffDepth3(Double.parseDouble(tsectTokens[8]));
-										//MORE TO WRITE HERE!!!
-										
+										tsect.setIntsQtr(Integer.parseInt(tsectTokens[9]));
+										tsect.setInts1(Integer.parseInt(tsectTokens[10]));
+										tsect.setInts3(Integer.parseInt(tsectTokens[11]));
 										//calculations per transect
 										perSlopeTot += tsect.getPerSlope();
 										tsectLitterAvg = (tsect.getLitterDepth1()+tsect.getLitterDepth2()+tsect.getLitterDepth3())/3;
 										standLitterAvgSum += tsectLitterAvg;
 										tsectDuffAvg =(tsect.getDuffDepth1()+tsect.getDuffDepth2()+tsect.getDuffDepth3())/3;
 										standDuffAvgSum += tsectDuffAvg;
-										
-										//test output
-										System.out.println("     " +tsect.getStandId());
+										standNQtrIn += tsect.getIntsQtr();
+										standN1In += tsect.getInts1();
+										standN3In += tsect.getInts3();
 									}
 								}//end tsect else tsectFirstLn
 							}//end tsect while
 					tsectFirstLn = true;
 					tsectBr.close();
 					
+					//loop to process diameter data for each stand
+					while((dLine = dBr.readLine()) != null)
+					{
+						if(dFirstLn)
+							dFirstLn = false;
+						else
+						{
+							//get tokens for each set of diameters in line
+							String[] dTokens = dLine.split(DELIMITER);
+							if(stand.getStandId().equals(dTokens[0]))
+							{
+								//calculate sum of Diameters Squared for each stand
+								sumD2s += (Double.parseDouble(dTokens[2])*Double.parseDouble(dTokens[2]));
+								sumD2r += (Double.parseDouble(dTokens[3])*Double.parseDouble(dTokens[3]));
+							}
+						}//end d else dFirstLn
+							
+					}//end d while
+					dFirstLn = true;
+					dBr.close();
+					
 					//stand level calcs from tsects
-					perSlopeAvg = (double)perSlopeTot/(double)nTsects;
+					stand.setNTesects(nTsects);
+					perSlopeAvg = (double)perSlopeTot/(double)stand.getNTsects();
 					stand.setAvgPerSlope(perSlopeAvg);
 					stand.setC(BrownFuelsCalc.scfCalc(stand.getAvgPerSlope()));
-					stand.setAvgLitterDepth(standLitterAvgSum/nTsects);
-					stand.setAvgDuffDepth(standDuffAvgSum/nTsects);
+					stand.setAvgLitterDepth(standLitterAvgSum/stand.getNTsects());
+					stand.setAvgDuffDepth(standDuffAvgSum/stand.getNTsects());
+					//total tallies for 0.1 - 3" material
+					stand.setNQtrIn(standNQtrIn);
+					stand.setN1In(standN1In);
+					stand.setN3In(standN3In);
+					//total plane lengths
+					//transect lengths for 0-0.25" and 0.25-1.0" material are the same
+					stand.setNlQtrIn(stand.getPlane1()*stand.getNTsects());
+					stand.setNl1In(stand.getNlQtrIn());
+					stand.setNl3In(stand.getPlane3()*stand.getNTsects());
+					stand.setNl3p(stand.getPlaneThreeP()*stand.getNTsects());
+					//tons/acre calculations
 					stand.setTonsAcLitter(BrownFuelsCalc.tonsLitter(stand.getAvgLitterDepth()));
 					stand.setTonsAcDuff(BrownFuelsCalc.tonsDuff(stand.getAvgDuffDepth()));
+					stand.setTonsAcQtrIn(BrownFuelsCalc.nrcTonsQtrIn(stand.getNQtrIn(), stand.getNlQtrIn(), 
+							stand.getC()));
+					stand.setTonsAc1In(BrownFuelsCalc.nrcTons1in(stand.getN1In(), stand.getNl1In(), 
+							stand.getC()));
+					stand.setTonsAc3In(BrownFuelsCalc.nrcTons3in(stand.getN3In(), stand.getNl3In(),
+							stand.getC()));
+					stand.setTonsAc3pS(BrownFuelsCalc.nrcTons3pS(stand.getNl3p(), stand.getC(), sumD2s));
+					stand.setTonsAc3pR(BrownFuelsCalc.nrcTons3pS(stand.getNl3p(), stand.getC(), sumD2r));
+					stand.setTotTonsAc(stand.getTonsAcLitter()+stand.getTonsAcDuff()+stand.getTonsAcQtrIn()+
+							stand.getTonsAc1In()+stand.getTonsAc3In()+stand.getTonsAc3pS()+
+							stand.getTonsAc3pR());
 					
-					//test output
-					System.out.println("     nTescts = "+nTsects);
 					//write output line to file----------------------------------------------------------------
 					out.write(stand.getStandId()+","+stand.getAvgPerSlope()+","+stand.getC()+","+
 					stand.getAvgLitterDepth()+","+stand.getAvgDuffDepth()+","+stand.getTonsAcLitter()+
-					","+stand.getTonsAcDuff()+"\n");
+					","+stand.getTonsAcDuff()+","+stand.getTonsAcQtrIn()+","+stand.getTonsAc1In()+","+
+					stand.getTonsAc3In()+","+stand.getTonsAc3pS()+","+stand.getTonsAc3pR()+","+
+					stand.getTotTonsAc()+"\n");
 				}//end stand else standFirstLn
 			}//end stand while
-			//tsectBr.close();
 			standBr.close();
 			out.close();
+			//confirm processing
+			JOptionPane.showMessageDialog(null, "Processing Complete!\n"+nStands+" Stands processed.");
 		}//end try
 		catch(Exception e)
 		{
